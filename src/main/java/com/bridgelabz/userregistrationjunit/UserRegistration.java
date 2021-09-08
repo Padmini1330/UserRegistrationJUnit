fe900062b5703ef9c06996d92281faddd82919dc
padmini v <padmini.v@perfios.com>
2021-09-08 18:34:04 +0530

    ADDED: method to validate user's email address

--- src/main/java/com/bridgelabz/userregistrationjunit/UserRegistration.java
+++ src/main/java/com/bridgelabz/userregistrationjunit/UserRegistration.java
@@ -5,31 +5,36 @@ import java.util.regex.Pattern;
 
 public class UserRegistration 
 {
-	public boolean validateFirstName(String firstname) {
+	public boolean validateFirstName(String firstname) 
+	{
 
 		String firstNameRegex = "^[A-Z]{1}[a-zA-Z]{2,}$";
 		return firstname.matches(firstNameRegex);
 	}
 	
-	public boolean validateLastName(String lastname) {
+	public boolean validateLastName(String lastname) 
+	{
 
 		String lastNameRegex = "^[A-Z]{1}[a-zA-Z]{2,}$";
 		return lastname.matches(lastNameRegex);
 	}
 	
-	public boolean validateEmail(String email) {
+	public boolean validateEmail(String email) 
+	{
 
 		String emailRegex = "^[a-zA-Z]+[a-zA-Z0-9]*[_+-.]?[a-zA-Z0-9]+[@]{1}[a-zA-Z0-9]+[.]{1}[a-z]+[.]?[a-z]+$";
 		return email.matches(emailRegex);
 	}
 	
-	public boolean validateMobileNumber(String mobileNumber) {
+	public boolean validateMobileNumber(String mobileNumber) 
+	{
 
 		String mobileNumberRegex = "^[0-9]{2}\\s{1}[0-9]{10}$"; 
 		return mobileNumber.matches(mobileNumberRegex);
 	}
 	
-	public boolean validatePassword(String password) {
+	public boolean validatePassword(String password) 
+	{
 
 		String passwordRegex = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[@!#$%&?\"]).{8,}$"; 
 		return password.matches(passwordRegex);
