4bd216eeccdb09f650c5259a73d914a87c24f8ad
padmini v <padmini.v@perfios.com>
2021-09-08 19:55:14 +0530

    ADDED: junit testcases to test validateLastName method

--- src/test/java/com/bridgelabz/userregistrationjunit/UserRegistrationTest.java
+++ src/test/java/com/bridgelabz/userregistrationjunit/UserRegistrationTest.java
@@ -38,5 +38,35 @@ public class UserRegistrationTest
 		
 	}
 	
+	@Test
+	public void givenLastName_WhenProper_ShouldReturnTrue() 
+	{
+		
+		UserRegistration userRegistration = new UserRegistration();
+		boolean isValid = userRegistration.validateLastName("Sharma");
+		Assert.assertTrue(isValid);
+		
+	}
+	
+	@Test
+	public void givenLastName_WhenFirstLetterIsSmall_ShouldReturnFalse() 
+	{
+		
+		UserRegistration userRegistration = new UserRegistration();
+		boolean isInvalid = userRegistration.validateLastName("sharma");
+		Assert.assertFalse(isInvalid);
+		
+	}
+	
+	@Test
+	public void givenLastName_WhenLesserThanThreeCharacters_ShouldReturnFalse() 
+	{
+		
+		UserRegistration userRegistration = new UserRegistration();
+		boolean isInvalid = userRegistration.validateLastName("Sh");
+		Assert.assertFalse(isInvalid);
+		
+	}
+	
 
 }
