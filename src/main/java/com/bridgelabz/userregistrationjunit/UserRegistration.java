78794455bd0dda0a9753f2a23b79d90774175339
padmini v <padmini.v@perfios.com>
2021-09-08 17:49:25 +0530

    ADDED: initialized project structure

---
+++ src/main/java/com/bridgelabz/userregistrationjunit/UserRegistration.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.userregistrationjunit;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UserRegistration 
+{
+	
+	
+}
+
+
+
+
---
+++ src/test/java/com/bridgelabz/userregistrationjunit/UserRegistrationTest.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.userregistrationjunit;
+
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class UserRegistrationTest 
+{
+
+	
+
+}
