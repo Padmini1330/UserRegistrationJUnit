f998936f60763d3c18bd001524279f7f63359d64
padmini v <padmini.v@perfios.com>
2021-09-08 19:49:08 +0530

    ADDED: method to validate firstname

--- src/main/java/com/bridgelabz/userregistrationjunit/UserRegistration.java
+++ src/main/java/com/bridgelabz/userregistrationjunit/UserRegistration.java
@@ -11,29 +11,7 @@ public class UserRegistration
 		return firstname.matches(firstNameRegex);
 	}
 	
-	public boolean validateLastName(String lastname) {
 
-		String lastNameRegex = "^[A-Z]{1}[a-zA-Z]{2,}$";
-		return lastname.matches(lastNameRegex);
-	}
-	
-	public boolean validateEmail(String email) {
-
-		String emailRegex = "^[a-zA-Z]+[a-zA-Z0-9]*[_+-.]?[a-zA-Z0-9]+[@]{1}[a-zA-Z0-9]+[.]{1}[a-z]+[.]?[a-z]+$";
-		return email.matches(emailRegex);
-	}
-	
-	public boolean validateMobileNumber(String mobileNumber) {
-
-		String mobileNumberRegex = "^[0-9]{2}\\s{1}[0-9]{10}$"; 
-		return mobileNumber.matches(mobileNumberRegex);
-	}
-	
-	public boolean validatePassword(String password) {
-
-		String passwordRegex = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[@!#$%&?\"]).{8,}$"; 
-		return password.matches(passwordRegex);
-	}
 }
 
 
