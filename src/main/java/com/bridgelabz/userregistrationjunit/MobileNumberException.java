b43e010f7d651c0d8457881c97d0136f27143aa7
padmini v <padmini.v@perfios.com>
2021-09-10 23:55:16 +0530

    ADDED: MobileNumberException custom exception class to handle invalid, null and empty phonenumbers

---
+++ src/main/java/com/bridgelabz/userregistrationjunit/MobileNumberException.java
@@ -0,0 +1,18 @@
+package com.bridgelabz.userregistrationjunit;
+
+
+public class MobileNumberException extends Exception
+{
+	enum MobileNumberExceptionType 
+	{
+		MOBILENUMBER_NULL, MOBILENUMBER_EMPTY, MOBILENUMBER_INVALID
+	}
+
+	MobileNumberExceptionType type;
+
+	public MobileNumberException(MobileNumberExceptionType type ,String message) 
+	{
+		super(message);
+		this.type=type;
+	}
+}
