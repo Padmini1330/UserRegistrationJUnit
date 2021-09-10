679da2c76bcd8329c92b5aed0e1361b4ffee2d35
padmini v <padmini.v@perfios.com>
2021-09-10 23:20:45 +0530

    ADDED: FirstNameException custom exception class to handle invalid, null and empty firstnames

---
+++ src/main/java/com/bridgelabz/userregistrationjunit/FirstNameException.java
@@ -0,0 +1,17 @@
+package com.bridgelabz.userregistrationjunit;
+
+public class FirstNameException extends Exception 
+{
+	enum FirstNameExceptionType 
+	{
+		FIRSTNAME_NULL, FIRSTNAME_EMPTY, FIRSTNAME_INVALID
+	}
+
+	FirstNameExceptionType type;
+
+	public FirstNameException(FirstNameExceptionType type ,String message) 
+	{
+		super(message);
+		this.type=type;
+	}
+}
